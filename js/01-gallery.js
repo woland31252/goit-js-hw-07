fb7d13c6c8923a97f319c8e0f4c16fbb2c7cc681
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-01-24 17:36:19 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,5 +4,20 @@ import { galleryItems } from './gallery-items.js';
 // Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
 
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
 
+function createGallery(galleryItems) {
+    return galleryItems.map(image => {
+        return `<a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>`;
+    });
+    console.log(elem);
+}
+console.log(gallery)
