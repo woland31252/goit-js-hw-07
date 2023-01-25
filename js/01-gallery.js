6e6dd8f190f2f58aa630c675de27dd9f296cf3b0
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-01-25 17:26:34 +0200

    update tasks2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,21 +3,51 @@ import { galleryItems } from './gallery-items.js';
 // Завдання 1 - галерея зображень
 // Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
 
+const gallery = document.querySelector('.gallery');
+const createGallery = galleryItems.map((image) => {
+  return `
+  <div class = "gallery__item">
+      <a class="gallery__link" href="${image.original}">
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          data-source="${image.original}"
+          alt="${image.description}"
+        />
+      </a>
+  </div>
+  `;
+  })
+  .join('');
+gallery.insertAdjacentHTML('beforeend', createGallery);
+gallery.addEventListener('click', showGallery);
 
-// console.log(galleryItems);
-const gallery = document.querySelector(".gallery");
+function showGallery(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
 
-function createGallery(galleryItems) {
-    return galleryItems.map(image => {
-        return `<a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>`;
-    });
-    console.log(elem);
+  const largImage = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" 
+    width="800"
+    height="600"/>`,
+    {
+      onShow: instance => {
+  document.onkeydown = function (e) {
+    e.preventDefault();
+    if (e.code == 'Escape') {
+      instance.close();
+    }
+  };
 }
+    }
+  );
+  largImage.show();
+  console.log()
+}
+
+console.log(galleryItems);
+
+
 console.log(gallery)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log()
+const gallery = document.querySelector('.gallery');
+const createGallery = galleryItems.map((image) => {
+  return `
+      <a class="gallery__link" href="${image.original}">
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          alt="${image.description}"
+        />
+      </a>
+  `;
+  })
+  .join('');
+gallery.insertAdjacentHTML('beforeend', createGallery);
+gallery.addEventListener('click', showGallery);
+
 
 console.log(galleryItems);
