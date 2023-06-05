62888d67990b019fc44d13b84465f563fcfef470
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-06-05 19:06:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,50 +1,41 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
-// Завдання 1 - галерея зображень
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
 
 const gallery = document.querySelector('.gallery');
-const createGallery = galleryItems.map((image) => {
-  return `
-  <div class = "gallery__item">
-      <a class="gallery__link" href="${image.original}">
-        <img
-          class="gallery__image"
-          src="${image.preview}"
-          data-source="${image.original}"
-          alt="${image.description}"
-        />
-      </a>
-  </div>
-  `;
-  })
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const liImg = galleryItems
+  .map(
+    elem => `<li class="gallery_item"><a class="gallery__link" href="${elem.original}"><img class="gallery__image"
+      src="${elem.preview}"
+      data-source="${elem.original}"
+      alt="${elem.description}"/></a></li>`
+  )
   .join('');
-gallery.insertAdjacentHTML('beforeend', createGallery);
-gallery.addEventListener('click', showGallery);
+gallery.innerHTML = liImg;
+gallery.addEventListener('click', handleImgRender);
 
-function showGallery(evt) {
-  evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
+function handleImgRender(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-
-  const largImage = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}" 
-    width="800"
-    height="600"/>`,
+  const instan = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
     {
-      onShow: instance => {
-  document.onkeydown = function (e) {
-    e.preventDefault();
-    if (e.code == 'Escape') {
-      instance.close();
-    }
-  };
-}
+      onShow: instan => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: instan => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
     }
   );
-  largImage.show();
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape' && basicLightbox.visible()) {
+      instan.close();
+    }
+  }
+  instan.show();
 }
-
 console.log(galleryItems);
-console.log(gallery)
